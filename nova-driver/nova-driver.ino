a3e84df1f53dbae90e7c321b7f0e4be1054e1fd5
Sean Fish <seantfish@gmail.com>
2022-02-19 02:07:40 -0500

    Added motor driver

---
+++ nova-driver/nova-driver.ino
@@ -0,0 +1,34 @@
+/*
+  GITMRG Nova Custom Trolling Motor Driver
+
+  Interfaces with PWM Module through digital potentiometer
+  PWM Module is interfaced with NV-series 8-speed trolling motor.  
+
+  v0.1
+  modified 20 FEB 2022
+  by Sean Fish
+
+  Roadmap
+   - Version 0: Arduino Nano - Serial Commuication
+   - Version 1: Arduino Nano RP2040 - micro-ROS
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+
+// PINS -------------------------------------------------------------
+const int pot_cs = 5;
+const int pot_incr = 6;
+const int pot_ud = 7;
+
+// 
+
+
+void setup() {
+  // 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
