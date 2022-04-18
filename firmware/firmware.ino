33bf8c77229c92a12e0f20dd44a8729c870f5745
Sean Fish <seantfish@gmail.com>
2022-04-18 18:32:20 -0400

    Started adding motor controller functions

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,5 +1,7 @@
 #include <LapX9C10X.h>
 
+#include <micro_ros_arduino.h>
+
 /*
   GITMRG Nova Custom Trolling Motor Driver
 
@@ -19,29 +21,70 @@
 */
 
 // PINS -------------------------------------------------------------
-const int THROT_CS_PIN = 5;
-const int THROT_UD_PIN = 6;
-const int THROT_INC_PIN = 7;
-const int DIR_SEL0_PIN = 2;
-const int DIR_SEL1_PIN = 3;
+// RC INPUT
+const int ORX_AUX1_PIN = 1; // checks if killed - need to figure out reset check
+const int ORX_GEAR_PIN = 2; // Auto vs Manual
+const int ORX_RUDD_PIN = 3; // yaw
+const int ORX_ELEV_PIN = 4; // WAM-V translate forward / backward
+const int ORX_AILE_PIN = 5; // WAM-V translate left / right
+const int ORX_THRO_PIN = 6;
+
+// MOTOR ALFA
+const int A_THRO_CS_PIN = 22;
+const int A_THRO_UD_PIN = 23;
+const int A_THRO_INC_PIN = 24;
+const int A_DIR_SEL0_PIN = 26;
+const int A_DIR_SEL1_PIN = 27;
+// MOTOR BRAVO
+const int B_THRO_CS_PIN = 28;
+const int B_THRO_UD_PIN = 29;
+const int B_THRO_INC_PIN = 30;
+const int B_DIR_SEL0_PIN = 32;
+const int B_DIR_SEL1_PIN = 33;
+// MOTOR CHARLIE
+
+// MOTOR DELTA
+
+
+// LIGHT TOWER
 
 
 // VARS -------------------------------------------------------------
-const int THROT_RESISTANCE = LAPX9C10X_X9C104;
+const int THRO_RESISTANCE = LAPX9C10X_X9C104;
 const int throttleMax = 50;
-int direction = 0; // 0 - OFF | 1 - FWD | 2 - BWD
 int m_signal=0;
+bool debug = true;
+int control_state = 1; // 0 - KILLED | 1 - STANDBY | 2 - MANUAL | 3 - AUTONOMOUS | 4 - AUXILIARY
+// add a blinking delay for restoring power after kill state 
+
 
 // DEVICES ----------------------------------------------------------
-LapX9C10X throttle(THROT_INC_PIN, THROT_UD_PIN, THROT_CS_PIN, THROT_RESISTANCE);
+// MOTOR ALFA
+LapX9C10X a_throttle(A_THRO_INC_PIN, A_THRO_UD_PIN, A_THRO_CS_PIN, THRO_RESISTANCE);
+int a_direction = 0; // 0 - OFF | 1 - FWD | 2 - BWD
+// MOTOR BRAVO
+LapX9C10X b_throttle(B_THRO_INC_PIN, B_THRO_UD_PIN, B_THRO_CS_PIN, THRO_RESISTANCE);
+int b_direction = 0;
+// MOTOR CHARLIE
+
+// MOTOR DELTA
+
+// FUNCTIONS --------------------------------------------------------
+void subscription_callback(const void * msgin) {
+  Serial.println("CALLBACK");
+}
+
+// Need function for translating RC to motor
+// Need publish vehicle state
 
 void setup() {
   Serial.begin(9600);
-  pinMode(DIR_SEL0_PIN, OUTPUT);
-  pinMode(DIR_SEL1_PIN, OUTPUT);
+  pinMode(A_DIR_SEL0_PIN, OUTPUT);
+  pinMode(A_DIR_SEL1_PIN, OUTPUT);
   Serial.println("NOVA MOTOR STARTING...");
   throttle.begin(-1); // Min resistance
   delay(5000);
+  debug = false;
 
   Serial.println("==================================================");
   Serial.println("============ NOVA MOTOR INIT COMPLETE ============");
